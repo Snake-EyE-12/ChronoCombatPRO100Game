c19d84257dfb624770b9ad6479a0649eb3189bd3
DPartney <dpartney@student.neumont.edu>
2023-11-13 18:55:07 -0700

    Basic Card Test

---
+++ Assets/_Project/Scripts/CardBehavior.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardBehavior : MonoBehaviour
+{
+    int CastFireball()
+    {
+        return 15;
+    }
+
+    int Strike()
+    {
+        return 6;
+    }
+
+    bool EquipWizardStaff()
+    {
+        return true;
+    }
+}
