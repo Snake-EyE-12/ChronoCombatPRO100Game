943fd9f2262a9cdc84e7607cc49cfb01e48b3555
Snake-EyE-12 <129359000+Snake-EyE-12@users.noreply.github.com>
2023-11-15 18:56:00 -0700

    Created Card Menu and Settings Thing

--- Assets/_Project/Scripts/CardDisplay.cs
+++ Assets/_Project/Scripts/CardDisplay.cs
@@ -1,8 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardDisplay : MonoBehaviour
 {
-    
+    [SerializeField] private Card card;
+    [SerializeField] private Image cardSprite;
+    public void CollectCard() {
+        
+    }
 }
