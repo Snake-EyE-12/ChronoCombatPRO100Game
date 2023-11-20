a8c3e61ff712fe606abb795f08c8e7fba79319ac
Snake-EyE-12 <129359000+Snake-EyE-12@users.noreply.github.com>
2023-11-20 16:19:27 -0700

    Updated slots
    Just realized I named it the past date

--- Assets/_Project/Scripts/InventoryManager.cs
+++ Assets/_Project/Scripts/InventoryManager.cs
@@ -7,7 +7,8 @@ public class InventoryManager : MonoBehaviour
     public const int HEAD = 0;
     public const int CHEST = 1;
     public const int HAND = 2;
-    public InventorySlot[] slots = new InventorySlot[3];
+    public const int BACKPACK = 3;
+    public InventorySlot[] slots = new InventorySlot[4];
     public void EquipItem(int slot, Item item) {
         slots[slot].swap(item);
     }
