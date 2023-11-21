8a1cea0519e095bbdb72296ac4e11d97e2bf5a4e
Snake-EyE-12 <129359000+Snake-EyE-12@users.noreply.github.com>
2023-11-20 18:05:59 -0700

    Merging

--- Assets/_Project/Scripts/Enemy.cs
+++ Assets/_Project/Scripts/Enemy.cs
@@ -14,53 +14,45 @@ public class Enemy : MonoBehaviour
     //basic stats
     public int maxHp;
     public int hp;
-    int def;
-    int atk;
-    
-    public int Attack() {
+    public int def;
+    public int atk;
+
+    void Attack() {
         //
-        int r = 1;
-
-        switch (r)
-        {
-            case 1: return Punch();
-                break;
-
-            case 2: 
-            return LowBlow();
-             break;
-        }
-
-        return -1;
-    }
-
-    int Punch() {
-        return atk + 1;
-    }
-
-    int LowBlow() {
-        return 10000000;
-
+        CombatController.Instance().DealDamageToPlayer(69);
     }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
     public void takeDamage(int damage) {
         hp -= damage;
         if(hp <= 0) die();
         healthBar.slider.value = hp;
     }
+
+
+    #region methods_for_things
     public void die() {
 
     }
+
+
+    protected int RandomMove(int min, int max)
+    {
+
+        int num = Random.Range(min, max);
+
+        return num;
+    }
+    #endregion
 }
